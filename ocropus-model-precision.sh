94e855a5f6873c52b6ad960979e529b6ba44606f
Jesper Zedlitz <jze@informatik.uni-kiel.de>
2017-03-30 13:45:54 +0200

    optional parameter to use the 'testing' directory only

--- ocropus-model-precision.sh
+++ ocropus-model-precision.sh
@@ -10,6 +10,9 @@ for m in *.pyrnn.gz; do
 	echo $m
 	n=`echo "$m" | sed 's/^.*-0//' | sed 's/.pyrnn.gz//'`
 	p1=`ocropus-precision.sh $m testing |tail -n1` 
-	p2=`ocropus-precision.sh $m training |tail -n1` 
+	
+	if [ "$1" != "testing" ] ; then
+		p2=`ocropus-precision.sh $m training |tail -n1` 
+	fi
 	echo "$n\t$p1\t$p2"  >> errors.csv
 done
